9b9a7875a145dea82cbdfad72a366b426893e89d
axaggg444 <alexander@spaan-online.de>
2022-09-01 17:14:15 +0200

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -3,5 +3,9 @@ from ursina.prefabs.health_bar import HealthBar
 
 class Zombie:
     def __init__(self):
-        model="cube"
-        Bar = HealthBar(text="Hi")
+        self.model="cube",
+        self.color = color.blue,
+
+
+
+        
