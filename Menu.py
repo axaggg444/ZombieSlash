017c1639eedd581a2b134f3df4c0d90042229476
axaggg444 <alexander@spaan-online.de>
2022-09-18 18:57:56 +0200

    Wrote Commentary for the Menu

--- Menu.py
+++ Menu.py
@@ -1,19 +1,26 @@
+#The Main Menu
+#import
 from ursina import *
 import subprocess
 
+#app
 app = Ursina()
 
+#Functions
 def Quit():
     sys.exit(0)
 
 def Start():
     subprocess.call("python Game.py")
 
+#window configuration
 window.borderless = False
 window.title = "Menu"
 window.icon = "assets/textures/icon.ico"
 window.fps_counter.visible = False
 window.exit_button.visible = False
+
+#Background image
 img = Entity(model="cube", texture="assets/textures/icon.png", scale=(10, 10, 10), rotate_z=.45)
 
 #Start Button
@@ -22,7 +29,8 @@ StartButton = Button(text="Start", scale=0.1, color=color.gray, y=.2)
 StartButton.on_click = Start
 
 #Quit Button
-#Works fine
 QuitButton = Button(text="Quit", scale=0.1, color=color.gray, y=-.2)
 QuitButton.on_click = application.quit
+
+#mainloop
 app.run()
